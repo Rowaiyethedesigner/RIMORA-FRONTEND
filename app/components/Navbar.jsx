2a36ff93c3b672801a88535d41ba6d0c92ff8f6f
IFELOLUWA AYOMIDE ROWAIYE <Rimoraproperties@gmail.com>
2025-12-30 10:21:05 +0100

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,13 +4,8 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
+        <div className="navbar-logo">RIMORA</div>
 
-        {/* Logo */}
-        <div className="navbar-logo">
-          RIMORA
-        </div>
-
-        {/* Links */}
         <ul className="navbar-links">
           <li><a href="/">Home</a></li>
           <li><a href="/about">About us</a></li>
@@ -22,29 +17,7 @@ export default function Navbar() {
             </a>
           </li>
         </ul>
-
       </div>
     </nav>
-  );
-}
-// app/components/Navbar.jsx
-export default function Navbar() {
-  return (
-    <header className="nav">
-      <div className="container nav-inner">
-        <div className="brand">
-          <h1>Rimora Properties</h1>
-          <div className="tag">FCDA-approved land in Abuja</div>
-        </div>
-
-        <nav className="nav-links">
-          <a href="/">Home</a>
-          <a href="/about">About us</a>
-          <a href="/properties">Our properties</a>
-          <a href="/contact">Contact</a>
-          <a className="btn" href="/contact">Book an inspection</a>
-        </nav>
-      </div>
-    </header>
   );
 }
