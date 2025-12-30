6d270632a932c0683b7ed825800808442df81759
IFELOLUWA AYOMIDE ROWAIYE <Rimoraproperties@gmail.com>
2025-12-30 20:57:16 +0100

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,22 +1,28 @@
-// app/components/Navbar.jsx
-
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="navbar-logo">RIMORA</div>
 
-        <ul className="navbar-links">
+        {/* LEFT: Logo */}
+        <div className="navbar-logo">
+          RIMORA
+        </div>
+
+        {/* CENTER: Links */}
+        <ul className="navbar-center-links">
           <li><a href="/">Home</a></li>
           <li><a href="/about">About us</a></li>
           <li><a href="/properties">Featured properties</a></li>
           <li><a href="/contact">Contact</a></li>
-          <li>
-            <a href="/contact" className="navbar-button">
-              Book an inspection
-            </a>
-          </li>
         </ul>
+
+        {/* RIGHT: CTA */}
+        <div className="navbar-cta">
+          <a href="/contact" className="navbar-button">
+            Book an inspection
+          </a>
+        </div>
+
       </div>
     </nav>
   );
